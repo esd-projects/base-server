9430f32043de827c12899ff3e06ddc9459cf3add
zhangjincheng <zhangjincheng@huidr.com>
2019-05-06 15:57:38 +0800

    DI容器

--- src/Plugins/Config/BaseConfig.php
+++ src/Plugins/Config/BaseConfig.php
@@ -130,6 +130,7 @@ class BaseConfig
         foreach ($this->reflectionClass->getProperties() as $property) {
             if ($property->getDeclaringClass()->getName() != BaseConfig::class) {
                 $varName = $property->getName();
+                if ($property->isPrivate()) continue;
                 if ($this->$varName != null) {
                     if (is_array($this->$varName)) {
                         foreach ($this->$varName as $key => $value) {
