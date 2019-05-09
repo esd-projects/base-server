bcfeb5058e9d070fcda8c6271294d602b31a284a
zhangjincheng <zhangjincheng@huidr.com>
2019-05-09 18:10:26 +0800

    DI容器

--- src/Plugins/DI/DIPlugin.php
+++ src/Plugins/DI/DIPlugin.php
@@ -31,23 +31,6 @@ class DIPlugin extends AbstractPlugin
         return "DI";
     }
 
-    private function clear_dir($path = null)
-    {
-        if (is_dir($path)) {    //判断是否是目录
-            $p = scandir($path);     //获取目录下所有文件
-            foreach ($p as $value) {
-                if ($value != '.' && $value != '..') {    //排除掉当./和../
-                    if (is_dir($path . '/' . $value)) {
-                        $this->clear_dir($path . '/' . $value);    //递归调用删除方法
-                        rmdir($path . '/' . $value);    //删除当前文件夹
-                    } else {
-                        unlink($path . '/' . $value);    //删除当前文件
-                    }
-                }
-            }
-        }
-    }
-
     /**
      * 在服务启动前
      * @param Context $context
@@ -59,18 +42,14 @@ class DIPlugin extends AbstractPlugin
         //有文件操作必须关闭全局RuntimeCoroutine
         enableRuntimeCoroutine(false);
         $serverConfig = Server::$instance->getServerConfig();
-        $cacheProxiesDir = $serverConfig->getBinDir() . '/cache/proxies';
-        if (file_exists($cacheProxiesDir)) {
-            $this->clear_dir($cacheProxiesDir);
-            rmdir($cacheProxiesDir);
+        $cacheProxiesDir = $serverConfig->getCacheDir() . '/proxies';
+        if (!file_exists($cacheProxiesDir)) {
+            mkdir($cacheProxiesDir, 0777, true);
         }
-        mkdir($cacheProxiesDir, 0777, true);
-        $cacheDir = $serverConfig->getBinDir() . "/cache/di";
-        if (file_exists($cacheDir)) {
-            $this->clear_dir($cacheDir);
-            rmdir($cacheDir);
+        $cacheDir = $serverConfig->getCacheDir() . "/di";
+        if (!file_exists($cacheDir)) {
+            mkdir($cacheDir, 0777, true);
         }
-        mkdir($cacheDir, 0777, true);
         $builder = new ContainerBuilder();
         $builder->enableCompilation($cacheDir);
         $builder->writeProxiesToFile(true, $cacheProxiesDir);
