4aaeb6d2c56fa3219bbeff76c0dd5a0167ac8f58
zhangjincheng <zhangjincheng@huidr.com>
2019-05-22 09:57:36 +0800

    ESD

--- src/Memory/CrossProcess/Table.php
+++ src/Memory/CrossProcess/Table.php
@@ -16,7 +16,7 @@ namespace ESD\BaseServer\Memory\CrossProcess;
  * Class Table
  * @package ESD\BaseServer\Memory
  */
-class Table
+class Table implements \Iterator, \Countable
 {
     /**
      * 默认为4个字节，可以设置1，2，4，8一共4种长度
@@ -150,4 +150,59 @@ class Table
         return $this->swooleTable->del($key);
     }
 
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->swooleTable->current();
+    }
+
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        return $this->swooleTable->next();
+    }
+
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->swooleTable->key();
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link https://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return $this->swooleTable->valid();
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        return $this->swooleTable->rewind();
+    }
 }
